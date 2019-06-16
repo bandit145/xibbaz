28a93c748297ed9fae977fb6994193784d2fbf7f
Philip Bove <phil@bove.online>
2019-06-15 21:09:36 -0400

    chaned how objects work

--- tests/unit/test_objects.py
+++ tests/unit/test_objects.py
@@ -6,12 +6,13 @@ import logging
 def test_template_object():
 	zab_obj = Template(**mock_data.template_do_request)
 	new_zab_obj = Template(**mock_data.template_do_request)
-	unchanged_obj = Template(**mock_data.template_do_request)
 	new_zab_obj.description = 'test'
 	assert zab_obj.host == 'test_template1'
-	assert zab_obj.items[0]['itemid'] == "28533"
-	assert zab_obj.items[1]['itemid'] == "28534"
-	assert zab_obj.is_changed(new_zab_obj)
-	assert not zab_obj.is_changed(unchanged_obj)
+	logging.debug(zab_obj.items[0].__dict__)
+	assert zab_obj.items[0].obj_id == "28533"
+	assert zab_obj.items[1].obj_id == "28534"
+	assert zab_obj.is_changed(new_zab_obj)[0].update
+	zab_obj = Template(**mock_data.template_do_request)
+	assert not zab_obj.is_changed(zab_obj)[1]
 
 	
--- xibbaz/src/objects.py
+++ xibbaz/src/objects.py
@@ -1,126 +1,145 @@
 import xibbaz.src.exceptions as exceptions
-
-ZABBIX_ITEM_TYPES = {
-    'zabbix_agent': 0,
-    'snmpv1_agent': 1,
-    'zabbix_trapper': 2,
-    'simple_check': 3,
-    'snmpv2_agent': 4,
-    'zabbix_internal': 5,
-    'snmpv3_agent': 6,
-    'zabbix_agent_active': 7,
-    'zabbix_aggregate': 8,
-    'web_item': 9,
-    'external_check': 10,
-    'database_monitor': 11,
-    'ipmi_agent': 12,
-    'ssh_agent': 13,
-    'telnet_agent': 14,
-    'calculated': 15,
-    'jmx_agent': 16,
-    'snmp_trap': 17,
-    'dependant_item': 18,
-    'http_agent': 19
-}
-
+import sys
 
 class ZabbixObject:
 
     fields = []
     sub_items = []
+    item_dep_map ={}
     # key name zabbix returns on search
-    PARAM_MAP = {}
-    RETURN_ID_KEY = None
+    obj_id = None
+    update = False
+    create = False
     # actual ID key name for update/delete operations
     ID_KEY = None
 
     def __init__(self, **kwargs):
         try:
-            for item in self.fields:
-                self.__dict__[item] = None
-            self.id = None
+            if self.ID_KEY in kwargs.keys():
+                self.obj_id = kwargs[self.ID_KEY]
             for item in kwargs.keys():
                 if item in self.fields:
                     setattr(self, item, kwargs[item])
                 elif item in self.sub_items:
                     # rename item to class name
-                    item_name = item
+                    item_name = list(item)
                     item_name[0] = item_name[0].upper()
                     del item_name[len(item_name)-1]
-                    sub_item_list = [ getattr(item_name)(**x) for x in kwargs['item']]
+                    item_name = ''.join(item_name)
+                    sub_item_list = [ getattr(sys.modules[__name__] ,item_name)(**x) for x in kwargs[item]]
                     setattr(self, item, sub_item_list)
+                else:
+                    setattr(self, item, None)
         except KeyError as error:
             raise exceptions.ZabbixPropertyException(error)
+
 
     # parse data into object
     def get_obj_data(self, id_req=False):
         obj_data =  {key: self.__dict__[key] for key in self.fields if self.__dict__[key]}
         if id_req:
-            obj_data[self.ID_KEY] = self.id
+            obj_data[self.ID_KEY] = self.obj_id
         return obj_data
 
+    # sanity check that item will be valid in new config state
+    def verify_integrity(self, zab_server):
+        pass
+
+    # returns self and self.update for easy sorting
     def is_changed(self, other_obj):
-        if self.get_obj_data() != other_obj.get_obj_data():
-            return True
-        return False
+        if self.get_obj_data(id_req=True) != other_obj.get_obj_data(id_req=True):
+            self.update = True
+        return self, self.update
 
 
-class ZabbixItem(ZabbixObject):
+class Item(ZabbixObject):
 
-    def __init__(self, name, **kwargs):
-            super().__init__(name, **kwargs)
-            self.type = None
+    TYPES = {
+        'zabbix_agent': 0,
+        'snmpv1_agent': 1,
+        'zabbix_trapper': 2,
+        'simple_check': 3,
+        'snmpv2_agent': 4,
+        'zabbix_internal': 5,
+        'snmpv3_agent': 6,
+        'zabbix_agent_active': 7,
+        'zabbix_aggregate': 8,
+        'web_item': 9,
+        'external_check': 10,
+        'database_monitor': 11,
+        'ipmi_agent': 12,
+        'ssh_agent': 13,
+        'telnet_agent': 14,
+        'calculated': 15,
+        'jmx_agent': 16,
+        'snmp_trap': 17,
+        'dependant_item': 18,
+        'http_agent': 19
+    }
 
-
-
-class AgentItem(ZabbixItem):
+    ID_KEY = 'itemid'
 
     fields = [
+        'type'
         'delay',
-        'key_',
         'hostid',
-        'name',
-        'type',
-        'value_type',
+        'interfaceid',
+        'key_',
+        'url',
+        'allow_traps',
+        'authtype',
         'description',
+        'error',
+        'follow_redirects',
+        'headers',
         'history',
-        'inventory_link',
-        'master_itemid'
-        'status',
-        'applications',
-        'preprocessing'
-    ]
-
-    def __init__(self, name, **kwargs):
-        super().__init__(name, api, **kwargs)
-        self.type = 0
-
-
-
-class SNMPV1Item(ZabbixObject):
-
-    fields = [
-        'delay',
-        'name',
-        'type',
-        'value_type',
-        'description',
-        'history',
-        'inventory_link',
-        'master_itemid',
-        'status',
+        'http_proxy',
+        'jmx_endpoint',
+        'output_format',
+        'params',
+        'password',
+        'port',
+        'post_type',
+        'posts',
+        'privatekey',
+        'publickey',
+        'query_fields',
+        'request_method',
+        'retrieve_mode',
         'snmp_community',
         'snmp_oid',
-        'applications',
-        'preprocessing'
+        'snmpv3_authpassphrase',
+        'snmpv3_authprotocol',
+        'snmpv3_ocntextname',
+        'snmpv3_privpassphrase',
+        'snmpv3_privprotocol',
+        'snmpv3_securitylevel',
+        'snmpv3_securityname',
+        'ssl_cert_file',
+        'ssl_key_file',
+        'ssl_key_password',
+        'status',
+        'status_codes',
+        'timeout',
+        'trapper_hosts',
+        'trends',
+        'units',
+        'username',
+        'verify_host',
+        'verify_peer'
     ]
 
-    #implements own init for snmp v1 vs v2
-    def __init__(self, name, api, logger, **kwargs):
-            super().__init__(name, api, logger)
-            self.type = 1
-           
+    def __init__(self, name, **kwargs):
+            super().__init__(**kwargs)
 
+
+class Application(ZabbixObject):
+    fields = [
+        'hosid',
+        'name'
+    ]
+    def __init__(self, name, **kwargs):
+            super().__init__(**kwargs)
 
 class Template(ZabbixObject):
 
@@ -136,19 +155,14 @@ class Template(ZabbixObject):
         'groups',
         'templates',
         'items',
+        'applications',
         'triggers'
     ]
 
     fields = [
         'host',
         'description',
-        'name',
-        'groups',
-        'templates',
-        'macros'
-        'discoveries',
-        'items',
-        'triggers'
+        'name'
     ]
 
     def __init__(self, **kwargs):
@@ -208,7 +222,6 @@ class Trigger(ZabbixObject):
     ]
     # Trigger depedendencies
     sub_items = [
-        'triggers',
         'tags'
     ]
     ID_KEY = 'triggerid'
